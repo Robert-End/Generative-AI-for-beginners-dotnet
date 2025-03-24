aa7ebdfd5890e985c31acd1dcbe0589d2bade12d
Robert-End <robert.korosin@endava.com>
2025-03-24 12:02:55 +0000

    initial for Azure AI

--- 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
+++ 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
@@ -1,12 +1,29 @@
-﻿using Azure;
+﻿using System.ClientModel;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Azure;
 using Azure.AI.Inference;
 using Microsoft.Extensions.AI;
 
-IChatClient client = new ChatCompletionsClient(
-        endpoint: new Uri("https://models.inference.ai.azure.com"),
-        new AzureKeyCredential(Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? throw new InvalidOperationException("Missing GITHUB_TOKEN environment variable. Ensure you followed the instructions to setup a GitHub Token to use GitHub Models.")))
-        .AsChatClient("Phi-3.5-MoE-instruct");
+var deploymentName = "gpt-4o-mini"; // e.g. "gpt-4o-mini"
+var endpoint = new Uri("https://ai-rkaistart496109111713.openai.azure.com/"); // e.g. "https://< your hub name >.openai.azure.com/"
+var apiKey = new ApiKeyCredential(Environment.GetEnvironmentVariable("AZURE_AI_SECRET"));
 
-var response = await client.GetResponseAsync("What is AI?");
+IChatClient client = new AzureOpenAIClient(
+    endpoint,
+    apiKey)
+.AsChatClient(deploymentName);
 
+// here we're building the prompt
+StringBuilder prompt = new StringBuilder();
+prompt.AppendLine("You will analyze the sentiment of the following product reviews. Each line is its own review. Output the sentiment of each review in a bulleted list and then provide a generate sentiment of all reviews. ");
+prompt.AppendLine("I bought this product and it's amazing. I love it!");
+prompt.AppendLine("This product is terrible. I hate it.");
+prompt.AppendLine("I'm not sure about this product. It's okay.");
+prompt.AppendLine("I found this product based on the other reviews. It worked for a bit, and then it didn't.");
+
+// send the prompt to the model and wait for the text completion
+var response = await client.GetResponseAsync(prompt.ToString());
+
+// display the repsonse
 Console.WriteLine(response.Message);
